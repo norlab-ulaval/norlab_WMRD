4e2b907aef1a517f98a8b8d12cc51d8cbf328892
DomBaril <dominic.baril@usherbrooke.ca>
2022-08-16 09:19:37 -0400

    parsed dataset for pytorch

--- eval/model_training.py
+++ eval/model_training.py
@@ -10,77 +10,86 @@ from models.kinematic.ICR_based import *
 from models.kinematic.Perturbed_unicycle import *
 from util.util_func import *
 
+from eval.torch_dataset import TorchWMRDataset
+
 from scipy.optimize import minimize
 
-dt = 0.05
-r = 0.175
-baseline = 1.296
-alpha = 0.8
-alpha_l = 0.8
-alpha_r = 0.8
-y_icr = 3.5
-y_icr_l = 3.5
-y_icr_r = -3.5
-x_icr = -2.5
-alpha_params = np.full((13), 1.0)
+train_dataset_path = '/home/dominic/repos/norlab_WMRD/data/husky/parsed_data/grass_1_torch.csv'
+training_horizon = 2
 
-icr_symmetrical = ICR_symmetrical(r, alpha, y_icr, dt)
-icr_assymetrical = ICR_assymetrical(r, alpha_l, alpha_r, x_icr, y_icr_l, y_icr_r, dt)
-perturbed_unicycle = Perturbed_unicycle(r, baseline, alpha_params, dt)
+wmr_dataset = TorchWMRDataset(train_dataset_path, training_horizon)
 
-dynamic = wmrde.Predictor()
+print(wmr_dataset.__len__())
 
-dataset_path = '../data/fr2021/states_inputs/teachb_data.csv'
-
-inputs_states = pd.read_csv(dataset_path)
-
-inputs = inputs_states[['cmd_left_vel', 'cmd_right_vel']].to_numpy()
-positions = inputs_states[['icp_pos_x', 'icp_pos_y', 'icp_pos_z']].to_numpy()
-orientations_quaternion = inputs_states[['icp_quat_x', 'icp_quat_y', 'icp_quat_z', 'icp_quat_w']].to_numpy()
-orientations_euler = np.zeros((orientations_quaternion.shape[0], 3))
-gravity_vectors = np.zeros((orientations_quaternion.shape[0], 3))
-for i in range(0, orientations_quaternion.shape[0]):
-    orientations_euler[i, :] = quaternion_to_euler(orientations_quaternion[i, :])
-    gravity_vectors[i, :] = euler_to_rotmat(orientations_euler[i, :])[:, 2]
-states = np.hstack((positions, orientations_euler))
-
-icp_ids = inputs_states['icp_index'].to_numpy()
-times = inputs_states['ros_time'].to_numpy()
-
-dt = 0.05
-horizon = 2.0 # seconds
-steps_per_prediction = math.floor(horizon / dt)
-
-prediction_weights = np.eye(6)
-prediction_weights_2d = np.eye(6)
-
-# TODO: Step to adjust model params here.
-
-
-def compute_prediction_error(init_params, model):
-    print(init_params)
-    model.adjust_motion_params(init_params)
-    prediction_error = 0
-    for i in range(0, inputs.shape[0]):
-        predicted_state = states[i, :]
-        prediction_end_id = 0
-        if i + steps_per_prediction >= inputs.shape[0]:
-            break
-        for j in range(0, steps_per_prediction):
-            predicted_state = model.predict(predicted_state, inputs[i+j, :])
-            if icp_ids[j] == icp_ids[i+steps_per_prediction]:
-                prediction_end_id = j
-                break
-            prediction_end_id = j
-        prediction_error += disp_err(states[prediction_end_id, :], predicted_state, prediction_weights)
-    print(prediction_error)
-    return prediction_error
+# dt = 0.05
+# r = 0.175
+# baseline = 1.296
+# alpha = 0.8
+# alpha_l = 0.8
+# alpha_r = 0.8
+# y_icr = 3.5
+# y_icr_l = 3.5
+# y_icr_r = -3.5
+# x_icr = -2.5
+# alpha_params = np.full((13), 1.0)
 #
-init_params = [0.8, 3.5] # for icr
-# init_params = np.full((13), 1.0)
-args = {'model': icr_symmetrical}
-# args = {'model': perturbed_unicycle}
-# test = compute_prediction_error(model=perturbed_unicycle, init_params=init_params)
-# test = compute_prediction_error(model=icr_symmetrical, init_params=init_params)
-minimize(compute_prediction_error, init_params, args=icr_symmetrical)
-# minimize(compute_prediction_error, init_params, args=perturbed_unicycle)
+# icr_symmetrical = ICR_symmetrical(r, alpha, y_icr, dt)
+# icr_assymetrical = ICR_assymetrical(r, alpha_l, alpha_r, x_icr, y_icr_l, y_icr_r, dt)
+# perturbed_unicycle = Perturbed_unicycle(r, baseline, alpha_params, dt)
+#
+# dynamic = wmrde.Predictor()
+#
+# dataset_path = '../data/fr2021/states_inputs/teachb_data.csv'
+#
+# inputs_states = pd.read_csv(dataset_path)
+#
+# inputs = inputs_states[['cmd_left_vel', 'cmd_right_vel']].to_numpy()
+# positions = inputs_states[['icp_pos_x', 'icp_pos_y', 'icp_pos_z']].to_numpy()
+# orientations_quaternion = inputs_states[['icp_quat_x', 'icp_quat_y', 'icp_quat_z', 'icp_quat_w']].to_numpy()
+# orientations_euler = np.zeros((orientations_quaternion.shape[0], 3))
+# gravity_vectors = np.zeros((orientations_quaternion.shape[0], 3))
+# for i in range(0, orientations_quaternion.shape[0]):
+#     orientations_euler[i, :] = quaternion_to_euler(orientations_quaternion[i, :])
+#     gravity_vectors[i, :] = euler_to_rotmat(orientations_euler[i, :])[:, 2]
+# states = np.hstack((positions, orientations_euler))
+#
+# icp_ids = inputs_states['icp_index'].to_numpy()
+# times = inputs_states['ros_time'].to_numpy()
+#
+# dt = 0.05
+# horizon = 2.0 # seconds
+# steps_per_prediction = math.floor(horizon / dt)
+#
+# prediction_weights = np.eye(6)
+# prediction_weights_2d = np.eye(6)
+#
+# # TODO: Step to adjust model params here.
+#
+#
+# def compute_prediction_error(init_params, model):
+#     print(init_params)
+#     model.adjust_motion_params(init_params)
+#     prediction_error = 0
+#     for i in range(0, inputs.shape[0]):
+#         predicted_state = states[i, :]
+#         prediction_end_id = 0
+#         if i + steps_per_prediction >= inputs.shape[0]:
+#             break
+#         for j in range(0, steps_per_prediction):
+#             predicted_state = model.predict(predicted_state, inputs[i+j, :])
+#             if icp_ids[j] == icp_ids[i+steps_per_prediction]:
+#                 prediction_end_id = j
+#                 break
+#             prediction_end_id = j
+#         prediction_error += disp_err(states[prediction_end_id, :], predicted_state, prediction_weights)
+#     print(prediction_error)
+#     return prediction_error
+# #
+# init_params = [0.8, 3.5] # for icr
+# # init_params = np.full((13), 1.0)
+# args = {'model': icr_symmetrical}
+# # args = {'model': perturbed_unicycle}
+# # test = compute_prediction_error(model=perturbed_unicycle, init_params=init_params)
+# # test = compute_prediction_error(model=icr_symmetrical, init_params=init_params)
+# minimize(compute_prediction_error, init_params, args=icr_symmetrical)
+# # minimize(compute_prediction_error, init_params, args=perturbed_unicycle)
---
+++ eval/torch_dataset.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class TorchWMRDataset(Dataset):
+    def __init__(self, csv_file, training_horizon=2, steady_state_window = 160, rate=20):
+        self.data = pd.read_pickle(csv_file)
+        self.training_horizon = training_horizon
+        self.steady_state_window = steady_state_window
+        self.rate = 20
+        self.timestep = 1/self.rate
+        self.steady_state_length = self.steady_state_window / self.timestep
+        self.horizons_per_step = np.floor(self.steady_state_length / self.training_horizon)
+
+        input = self.data.drop(['gt_icp_x', 'gt_icp_y', 'gt_icp_yaw'], axis=1).values
+        output = self.data[['gt_icp_x', 'gt_icp_y', 'gt_icp_yaw']].values
+
+        self.x_train = torch.tensor(input)
+        self.y_train = torch.tensor(output)
+
+    def __len__(self):
+        return len(self.y_train)
+
+    def __getitem__(self, idx):
+        return self.x_train[idx], self.y_train[idx]
+
