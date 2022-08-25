01776f601e91fcbc8b2a8e3ba3f6e8f5bc1e6f76
DomBaril <dominic.baril@usherbrooke.ca>
2022-08-25 16:11:13 -0400

    progress on automatic bag extraction script

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -13,33 +13,33 @@ csv_file=$results_path"/data".csv
 traj_file=$results_path"/traj".vtk
 map_file=$results_path"/map".vtk
 
-# start nodes and pause playing
-roscore &
-rosbag play bag_path rate:=0.5 --clock &
-sleep 5
-rosservice call /play/pause_playback "data: true"
-roslaunch husky_mapping realtime_mapping.launch csv_file_name:=$csv_file &
-roslaunch pose_cmd_logger logger.launch &
+#echo $csv_file
+
+## start nodes and pause playing
+roslaunch norlab_imu_tools husky_imu_and_wheel_odom.launch&
+roslaunch husky_mapping realtime_mapping.launch&
+roslaunch pose_cmds_logger logger.launch &
+roslaunch norlab_bag_player husky.launch bagfile:=$bag_path rate:=0.5&
 rviz &
 sleep 5
 rosservice call /play/pause_playback "data: false"
 
-# wait until the end of the bag
+## wait until the end of the bag
 while [[ ! -z `pgrep play` ]]
 do
 sleep 1
 done
 
-# save the map
+## save the map
 sleep 10
 rosservice call /save_map "map_file_name:
-data: '$map_file'"
+#data: '$map_file'"
 rosservice call /save_trajectory "trajectory_file_name:
 data: '$trajectory_file'"
 rosservice call /save_data "trajectory_file_name:
 data: '$data_file'"
-
-# kill everything
+#
+## kill everything
 killall rviz
 killall pointcloud2_deskew_node
 killall mapper_node
