b32ead9dd2395ac3bb30d0e7ed6960bcc692d8eb
Mike Simard <michael.simard@usherbrooke.ca>
2022-09-09 11:36:44 -0400

    Added quivers

--- MS/GPS Pipeline/test.py
+++ MS/GPS Pipeline/test.py
@@ -17,11 +17,12 @@ def plot_utm_coordinates(utm_lattitudes, utm_longitudes):
 
 def remove_min_value_from_all_elements_in_list(list_of_values):
     min_value = min(list_of_values)
+    print(min_value)
     for i in range(len(list_of_values)):
         list_of_values[i] = list_of_values[i] - min_value
     return list_of_values
 
-def import_gps_data_from_csv(csv_file_path):
+def import_gps_data_from_pos_file(csv_file_path):
     gps_data = pd.read_table(csv_file_path, sep="\s+", parse_dates={"Timestamp": [0, 1]}, error_bad_lines=False, skiprows=25)
     return gps_data
 
@@ -32,8 +33,8 @@ def get_utm_lattitudes_and_longitudes_from_gps_data(gps_data):
     
     utm_lattitudes, utm_longitudes = convert_list_of_lattitudes_and_longitudes_to_utm_coordinates(lattitudes, longitudes)
     
-    utm_lattitudes = remove_min_value_from_all_elements_in_list(utm_lattitudes)
-    utm_longitudes = remove_min_value_from_all_elements_in_list(utm_longitudes)
+    #utm_lattitudes = remove_min_value_from_all_elements_in_list(utm_lattitudes)
+    #utm_longitudes = remove_min_value_from_all_elements_in_list(utm_longitudes)
     
     return utm_lattitudes, utm_longitudes
 
@@ -104,7 +105,12 @@ def compute_angle_between_two_gps_data_arrays(utm_lattitudes_1, utm_longitudes_1
     return angle
 
 def plot_angles(angles):
-    plt.plot(angles)
+    plt.scatter(range(len(angles)), angles)
+    plt.show()
+
+def superpose_quiver_with_angles_on_utm_coordinates(utm_lattitudes, utm_longitudes, angles):
+    
+    plt.quiver(utm_lattitudes, utm_longitudes, np.cos(angles), np.sin(angles), angles, scale_units='xy', scale=0.5)
     plt.show()
 
 if __name__ == "__main__":
@@ -114,9 +120,9 @@ if __name__ == "__main__":
     complete_data['ros_time']  = convert_complete_data_ros_time_from_nanoseconds_to_seconds(complete_data).astype(int)
 
     # Import GPS data
-    front_gps_data = import_gps_data_from_csv('MS/GPS Pipeline/data_september8th/reach-raw_202209081407.pos')
-    middle_gps_data = import_gps_data_from_csv('MS/GPS Pipeline/data_september8th/reach_raw_202209081406.pos')
-    back_gps_data = import_gps_data_from_csv('MS/GPS Pipeline/data_september8th/raw_202209081407.pos')
+    front_gps_data = import_gps_data_from_pos_file('MS/GPS Pipeline/data_september8th/reach-raw_202209081407.pos')
+    middle_gps_data = import_gps_data_from_pos_file('MS/GPS Pipeline/data_september8th/reach_raw_202209081406.pos')
+    back_gps_data = import_gps_data_from_pos_file('MS/GPS Pipeline/data_september8th/raw_202209081407.pos')
     
 
     # Convert timestamps to unix ros time
@@ -124,24 +130,32 @@ if __name__ == "__main__":
     middle_gps_data['ros_time'] = convert_timestamp_to_unix_ros_time(middle_gps_data.Timestamp).astype(int)
     back_gps_data['ros_time'] = convert_timestamp_to_unix_ros_time(back_gps_data.Timestamp).astype(int)
 
+    # Keep common data between data sources
 
     common_unix_ros_time_array = compute_common_unix_ros_time_elements(front_gps_data, back_gps_data)
     front_gps_data, back_gps_data = keep_data_with_same_unix_ros_time_elements(front_gps_data, back_gps_data, common_unix_ros_time_array)
+    common_unix_ros_time_array = compute_common_unix_ros_time_elements(front_gps_data, middle_gps_data)
+    front_gps_data, middle_gps_data = keep_data_with_same_unix_ros_time_elements(front_gps_data, middle_gps_data, common_unix_ros_time_array)
 
+    # Convert GPS data to UTM coordinates
     front_utm_lattitudes, front_utm_longitudes = get_utm_lattitudes_and_longitudes_from_gps_data(front_gps_data)
     middle_utm_lattitudes, middle_utm_longitudes = get_utm_lattitudes_and_longitudes_from_gps_data(middle_gps_data)
     back_utm_lattitudes, back_utm_longitudes = get_utm_lattitudes_and_longitudes_from_gps_data(back_gps_data)
 
-
+    # TODO
     #duplication_indexes, front_utm_lattitudes, front_utm_longitudes = resample_utm_lattitudes_and_longitudes(front_utm_lattitudes, front_utm_longitudes, 20, 5)
     #duplication_indexes, front_utm_lattitudes, front_utm_longitudes = cut_utm_lattitudes_and_longitudes(duplication_indexes, front_utm_lattitudes, front_utm_longitudes, len(complete_data))
     
+    # Compute angles between two GPS data arrays
     angles = compute_angle_between_two_gps_data_arrays(front_utm_lattitudes, front_utm_longitudes, back_utm_lattitudes, back_utm_longitudes)
-    plot_angles(angles)
+    #plot_angles(angles)
+    superpose_quiver_with_angles_on_utm_coordinates(front_utm_lattitudes, front_utm_longitudes, angles)
 
     plot_utm_coordinates(front_utm_lattitudes, front_utm_longitudes)
     plot_utm_coordinates(middle_utm_lattitudes, middle_utm_longitudes)
     plot_utm_coordinates(back_utm_lattitudes, back_utm_longitudes)
+    
+    # Merge GPS data with complete data
     #merged_data =  merge_gps_data_in_complete_data(complete_data, utm_lat, utm_lon)
     #pd.to_pickle(merged_data, 'MS/GPS Pipeline/grass_1_full_with_positions.csv')
 
