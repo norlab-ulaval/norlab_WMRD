e0cc7fd1df8aa488ba98b79120e2f36bc7b7eee1
Mike Simard <michael.simard@usherbrooke.ca>
2022-09-22 12:04:01 -0400

    Clean version to test

--- MS/GPS Pipeline/test.py
+++ MS/GPS Pipeline/test.py
@@ -3,7 +3,14 @@ import utm
 import matplotlib.pyplot as plt
 import numpy as np
 
-# In order of utilization
+
+
+
+
+
+########################################################################################
+# Methods. Ordered by execution order
+########################################################################################
 def import_gps_data_from_pos_file(csv_file_path):
     gps_data = pd.read_table(csv_file_path, sep="\s+", parse_dates={"Timestamp": [0, 1]}, error_bad_lines=False, skiprows=25)
     return gps_data
@@ -54,43 +61,38 @@ def cut_utm_lattitudes_and_longitudes(duplication_indexes, utm_lattitudes, utm_l
     
     return duplication_indexes, utm_lattitudes, utm_longitudes
 
+def compute_angle_between_back_and_front_gps(utm_lattitudes_1, utm_longitudes_1, utm_lattitudes_2, utm_longitudes_2):
+    
+    angles = []
+    hardcoded_angle_between_the_two_gps_installed_on_robot = 16.48
+    
+    for i in range(len(utm_lattitudes_1)):
+        
+        x1 = utm_lattitudes_1[i]
+        x2 = utm_lattitudes_2[i]
+        y1 = utm_longitudes_1[i]
+        y2 = utm_longitudes_2[i]
+        
+        angle = (np.arctan2(y2 - y1, x2 - x1) * 180 / np.pi) - hardcoded_angle_between_the_two_gps_installed_on_robot
+        angles.append(angle)
+    
+    return angles
 
 
 
 
 
+
+########################################################################################
+# Utils for visualization
+########################################################################################
 def plot_utm_coordinates(utm_lattitudes, utm_longitudes):
     plt.plot(utm_lattitudes, utm_longitudes, '.')
     plt.show()
-
-
-def merge_lattitudes_and_longitudes_in_complete_data(complete_data, utm_lattitudes, utm_longitudes):
-    
-    complete_data['utm_lat'] = utm_lattitudes
-    complete_data['utm_lon'] = utm_longitudes
-    
-    return complete_data
-
-
-def get_absolute_angle_in_degrees_between_two_utm_coordinates(x1, y1, x2, y2):
-    angle = (np.arctan2(y2 - y1, x2 - x1) * 180 / np.pi)-16.48
-    return angle
-
-def compute_angle_between_two_gps_data_arrays(utm_lattitudes_1, utm_longitudes_1, utm_lattitudes_2, utm_longitudes_2):
-    
-    angle = []
-    
-    for i in range(len(utm_lattitudes_1)):
-        angle.append(get_absolute_angle_in_degrees_between_two_utm_coordinates(utm_lattitudes_1[i], utm_longitudes_1[i], utm_lattitudes_2[i], utm_longitudes_2[i]))
-    
-    return angle
-
 def plot_angles(angles):
     plt.plot(range(len(angles)), angles)
     plt.show()
-
 def superpose_quiver_with_angles_on_utm_coordinates(utm_lattitudes, utm_longitudes, angles):
-    
     plt.figure()
     plt.quiver(utm_lattitudes, utm_longitudes, np.cos(np.radians(angles)), np.sin(np.radians(angles)), np.radians(angles), scale_units='xy', scale=1.5)
     plt.title('Quiver plot')
@@ -99,6 +101,13 @@ def superpose_quiver_with_angles_on_utm_coordinates(utm_lattitudes, utm_longitud
     plt.show()
 
 
+
+
+
+
+########################################################################################
+# Main
+########################################################################################
 def main(complete_data_path, front_gps_data_path, middle_gps_data_path, back_gps_data_path, complete_data_sampling_rate, gps_data_sampling_rate):
 
     # Import data
@@ -134,34 +143,36 @@ def main(complete_data_path, front_gps_data_path, middle_gps_data_path, back_gps
     duplication_indexes, back_utm_latitudes, back_utm_longitudes = cut_utm_lattitudes_and_longitudes(duplication_indexes, back_utm_latitudes, back_utm_longitudes, len(complete_data))
 
     # Compute angles between two GPS data arrays
-    angle_between_front_and_back_gps = compute_angle_between_two_gps_data_arrays(front_utm_latitudes, front_utm_longitudes, back_utm_latitudes, back_utm_longitudes)
-    angle_between_front_and_middle_gps = compute_angle_between_two_gps_data_arrays(front_utm_latitudes, front_utm_longitudes, middle_utm_latitudes, middle_utm_longitudes)
-    angle_between_middle_and_back_gps = compute_angle_between_two_gps_data_arrays(middle_utm_latitudes, middle_utm_longitudes, back_utm_latitudes, back_utm_longitudes)
+    angle_between_front_and_back_gps = compute_angle_between_back_and_front_gps(front_utm_latitudes, front_utm_longitudes, back_utm_latitudes, back_utm_longitudes)
 
-    # Merge GPS data with complete data
-    #merged_data =  merge_gps_data_in_complete_data(complete_data, utm_lat, utm_lon)
-    #pd.to_pickle(merged_data, 'MS/GPS Pipeline/grass_1_full_with_positions.csv')
-
-
+    # Merge GPS data with complete data 
+    complete_data['front_gps_utm_latitudes'] = front_utm_latitudes
+    complete_data['front_gps_utm_longitudes'] = front_utm_longitudes
+    complete_data['middle_gps_utm_latitudes'] = middle_utm_latitudes
+    complete_data['middle_gps_utm_longitudes'] = middle_utm_longitudes
+    complete_data['back_gps_utm_latitudes'] = back_utm_latitudes
+    complete_data['back_gps_utm_longitudes'] = back_utm_longitudes
+    complete_data['angle_between_front_and_back_gps'] = angle_between_front_and_back_gps
+    complete_data['duplication_indexes'] = duplication_indexes
+    
     # Plot angles
     # Uncomment the following lines to plot angles
-    # plot_angles(angle_between_front_and_back_gps)
-    # superpose_quiver_with_angles_on_utm_coordinates(front_utm_latitudes, front_utm_longitudes, angle_between_front_and_back_gps)
-
+    #plot_angles(angle_between_front_and_back_gps)
+    #superpose_quiver_with_angles_on_utm_coordinates(front_utm_latitudes, front_utm_longitudes, angle_between_front_and_back_gps)
     #plot_utm_coordinates(front_utm_latitudes, front_utm_longitudes)
     #plot_utm_coordinates(middle_utm_latitudes, middle_utm_longitudes)
     #plot_utm_coordinates(back_utm_latitudes, back_utm_longitudes)
-
-    print('ALLO')
 
 
 if __name__ == "__main__":
 
     complete_data_path = "MS/GPS Pipeline/data_september8th/doughnut__2022-09-08-10-11-57.pkl"
-    front_gps_data_path = "MS/GPS Pipeline/data_september8th/reach-raw_202209081407.pos"
-    middle_gps_data_path = "MS/GPS Pipeline/data_september8th/reach_raw_202209081406.pos"
-    back_gps_data_path = "MS/GPS Pipeline/data_september8th/raw_202209081407.pos"
+    front_gps_path = "MS/GPS Pipeline/data_september8th/reach-raw_202209081407.pos"
+    middle_gps_path = "MS/GPS Pipeline/data_september8th/reach_raw_202209081406.pos"
+    back_gps_path = "MS/GPS Pipeline/data_september8th/raw_202209081407.pos"
+    new_complete_data_path = "MS/GPS Pipeline/grass_1_full_with_positions.pkl"
     complete_data_sampling_rate_in_hz = 20
     gps_data_sampling_rate_in_hz = 5
 
-    complete_data = main(complete_data_path, front_gps_data_path, middle_gps_data_path, back_gps_data_path, complete_data_sampling_rate_in_hz, gps_data_sampling_rate_in_hz)
+    complete_data = main(complete_data_path, front_gps_path, middle_gps_path, back_gps_path, complete_data_sampling_rate_in_hz, gps_data_sampling_rate_in_hz, new_complete_data_path)
+    pd.to_pickle(complete_data, new_complete_data_path)
